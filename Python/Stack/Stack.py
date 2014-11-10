983318ee498407fe50cad85d050f2cb427f6ebe4
Amit Kulkarni <yuvraj2987@gmail.com>
2014-11-10 07:27:20 -0800

    Python stack class

---
+++ Python/Stack/Stack.py
@@ -0,0 +1,32 @@
+class StackFullError(Exception):
+    pass
+
+
+class StackEmptyError(Exception):
+    pass
+
+
+class Stack:
+
+    def __init__(self, size):
+        self.items = []
+        self.size = size
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, element):
+        if len(self.items) == self.size:
+            raise StackFullError()
+        self.items.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            raise StackEmptyError()
+        return self.items.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            raise StackEmptyError()
+        return self.items[len(self.items) - 1]
+
